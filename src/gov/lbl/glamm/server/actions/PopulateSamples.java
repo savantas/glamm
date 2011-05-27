a6b71367a2cbfc5614d6f5a6982583b1eff8a065
jtbates <jtbates@lbl.gov>
2011-05-27 22:00:58 +0000

    Separated server-side actions into non-requesthandler and requesthandler packages

--- src/gov/lbl/glamm/server/actions/PopulateSamples.java
+++ src/gov/lbl/glamm/server/actions/PopulateSamples.java
@@ -1,19 +1,19 @@
-package gov.lbl.glamm.server.requesthandlers.impl;
+package gov.lbl.glamm.server.actions;
 
-import gov.lbl.glamm.client.model.Experiment;
+import gov.lbl.glamm.client.model.Sample;
 import gov.lbl.glamm.server.SessionManager;
 import gov.lbl.glamm.server.dao.ExperimentDAO;
 import gov.lbl.glamm.server.dao.impl.ExperimentDAOImpl;
 
 import java.util.ArrayList;
 
-public class PopulateExperiments {
+public class PopulateSamples {
 	
-	public static ArrayList<Experiment> populateExperiments(SessionManager sm, String taxonomyId) {
-	
+	public static ArrayList<Sample> populateSamples(SessionManager sm, String taxonomyId) {
+		
 		if(taxonomyId != null && !taxonomyId.isEmpty()) {
 			ExperimentDAO expDao = new ExperimentDAOImpl(sm);
-			return expDao.getAllExperiments(taxonomyId);
+			return expDao.getAllSamples(taxonomyId);
 		}
 	
 		return null;
