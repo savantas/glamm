7af8a5e157ac4190cd2b2d59f1cdc235a39b5fd8
jtbates <jtbates@lbl.gov>
2011-10-05 18:17:57 +0000

    Set map data before organism while populating RetrosynthesisPresenter. Removed MetabolicNetworkDAO and implementation.

--- src/gov/lbl/glamm/server/dao/MetabolicNetworkDAO.java
+++
@@ -1,7 +0,0 @@
-package gov.lbl.glamm.server.dao;
-
-import gov.lbl.glamm.client.model.MetabolicNetwork;
-
-public interface MetabolicNetworkDAO {
-//	public MetabolicNetwork getNetworkForMapId(String mapId);
-}
